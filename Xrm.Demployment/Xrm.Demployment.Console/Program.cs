fffdfa0656a518648c61c12d579974e1f81a94e8
Bartosz Pędziwiatr-Ratajczak <bartosz.pedziwiatr@gmail.com>
2018-02-19 20:59:50 +0100

    add configuration

--- Xrm.Demployment/Xrm.Demployment.Console/Program.cs
+++ Xrm.Demployment/Xrm.Demployment.Console/Program.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Xrm.Deployment.Core;
+using Xrm.Deployment.Core.Confg;
+using System.Linq;
 
 namespace Xrm.Demployment.Console
 {
@@ -11,14 +13,27 @@ namespace Xrm.Demployment.Console
     {
         private static void Main(string[] args)
         {
-            //var parser = new Parser(with => {
-            //    with.EnableDashDash = true;
-
-            //});
+            ConfigReader reader = new ConfigReader();
+            IDictionary<string,IConfigItem> configs = reader.Read();
+            if(configs != null  && configs.Count > 0 && args.Length <=0)
+            {
+                RunOnConfigDefault(configs.Values.First());
+                return;
+            }
+       
             var result = Parser.Default.ParseArguments<Options>(args)
                  .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
                  .WithNotParsed((errs) => HandleParseError(errs));
         }
+
+        private static void RunOnConfigDefault(IConfigItem configItem)
+        {
+            CrmServiceClient client = GetConnection(configItem);
+            AssemblyLoaderCrm loader = new AssemblyLoaderCrm(configItem.Path, client, configItem.IsolationMode, configItem.SourceType);
+            loader.Run();
+        }
+
+        
 
         private static void RunOptionsAndReturnExitCode(Options opts)
         {
@@ -34,6 +49,15 @@ namespace Xrm.Demployment.Console
         private static CrmServiceClient GetConnection(Options opts)
         {
             CrmServiceClient client = new CrmServiceClient(opts.ConnectionString);
+            if (!string.IsNullOrWhiteSpace(client.LastCrmError))
+            {
+                throw new ApplicationException($"CrmServieClient initalization failed with error {client.LastCrmError}");
+            }
+            return client;
+        }
+        private static CrmServiceClient GetConnection(IConfigItem confgiItem)
+        {
+            CrmServiceClient client = new CrmServiceClient(confgiItem.ConnectionString);
             if (!string.IsNullOrWhiteSpace(client.LastCrmError))
             {
                 throw new ApplicationException($"CrmServieClient initalization failed with error {client.LastCrmError}");
