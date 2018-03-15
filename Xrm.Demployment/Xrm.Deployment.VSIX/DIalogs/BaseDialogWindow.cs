332c0735bb881c9aa49129bb5e89849c69e5b563
Marcin Grabski <m.grabski89@hotmail.com>
2018-03-15 19:50:51 +0100

    clean code in vsix project

--- Xrm.Demployment/Xrm.Deployment.VSIX/DIalogs/BaseDialogWindow.cs
+++ Xrm.Demployment/Xrm.Deployment.VSIX/DIalogs/BaseDialogWindow.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.VisualStudio.PlatformUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Xrm.Deployment.VSIX.DIalogs
 {
     public class BaseDialogWindow : DialogWindow
     {
-
         public BaseDialogWindow()
         {
             this.HasMaximizeButton = true;
